f7337a9f6d17f46b0c9ff22adb2bc3bf641fffd5
unknown <pooja>
2025-03-29 12:04:33 +0530

    added 1 to 10 number

---
+++ sample/src/com/sample/tests.java
@@ -0,0 +1,15 @@
+package com.sample;
+
+import java.util.ArrayList;
+
+public class tests {
+public static void main(String[] args)
+{
+	ArrayList<String> l=new ArrayList<String>();
+	l.add("10");
+	l.add("20");
+	l.add("30");
+	System.out.println(l);
+	
+}
+}
