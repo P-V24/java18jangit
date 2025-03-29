382c5609e5d577dee8f8858c2a784a1213dc0b2a
unknown <pooja>
2025-03-29 17:16:51 +0530

    Show program

---
+++ s/src/com/s/pr.java
@@ -0,0 +1,16 @@
+package com.s;
+
+import java.util.ArrayList;
+
+public class pr {
+	public static void main(String[] args)
+	{
+		ArrayList<String> l=new ArrayList<String>();
+		l.add("10");
+		l.add("20");
+		l.add("30");
+		System.out.println(l);
+		
+	}
+
+}
