cc64fa72483332f9271ef7b658b3287a87340e38
unknown <pooja>
2025-03-29 13:35:40 +0530

    Hell0

---
+++ test1/src/com/test1/example.java
@@ -0,0 +1,16 @@
+package com.test1;
+
+import java.util.ArrayList;
+
+public class example {
+	public static void main(String[] args)
+	{
+		ArrayList<String> l=new ArrayList<String>();
+		l.add("10");
+		l.add("20");
+		l.add("30");
+		System.out.println(l);
+		
+	}
+
+}
