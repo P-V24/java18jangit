b2852877ed4604f3b16a10f20bbe786da0ebfe8e
unknown <pooja>
2025-03-29 11:01:40 +0530

    Arraylist Program

---
+++ Test/src/com/velocity/Sample.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Sample {
+	public static void main(String[] args)
+	{
+		ArrayList<String> l=new ArrayList<String>();
+		l.add("Ankita");
+		l.add("vivek");
+		l.add("pooja");
+		l.add("Neeta");
+		System.out.println(l);
+	}
+
+}
