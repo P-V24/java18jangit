f243c65f26ff41a284c224a569dbcd3f75a01c52
P-V24 <poojakhot49@gmail.com>
2025-03-30 11:56:20 +0530

    ArrayList code added for city

---
+++ program1/src/com/program1/Array.java
@@ -0,0 +1,10 @@
+package com.program1;
+
+public class Array {
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome git");
+		
+	}
+
+}
---
+++ program1/src/com/program1/test.java
@@ -0,0 +1,18 @@
+package com.program1;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+public static void main(String[] args)
+{
+	List<String> list=new ArrayList<>();
+	list.add("pune");
+	list.add("mumbai");
+	list.add("Bangaloure");
+	System.out.println(list);
+	
+	
+}
+}
