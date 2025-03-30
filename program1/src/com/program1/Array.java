608fadc6ece494e8089bf5cf818c7029967e695d
P-V24 <poojakhot49@gmail.com>
2025-03-30 17:19:36 +0530

    Program final

---
+++ program1/src/com/program1/Array.java
@@ -0,0 +1,17 @@
+package com.program1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Array {
+	public static void main(String[] args)
+	{
+		List<String> list=new ArrayList<>();
+		list.add("10");
+		list.add("30");
+		list.add("20");
+		System.out.println(list);
+		
+	}
+
+}
