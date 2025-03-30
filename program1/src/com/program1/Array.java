373b2ad0aac3e02f25b26c7c268bd7f89a377639
P-V24 <poojakhot49@gmail.com>
2025-03-30 12:06:08 +0530

    ArrayList code

--- program1/src/com/program1/Array.java
+++ program1/src/com/program1/Array.java
@@ -1,9 +1,16 @@
 package com.program1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Array {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome git");
+		List<String> list=new ArrayList<>();
+		list.add("10");
+		list.add("30");
+		list.add("20");
+		System.out.println(list);
 		
 	}
 
